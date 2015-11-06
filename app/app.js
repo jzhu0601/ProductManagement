e9150d2408dd7c5f6f7607ef79e3d72258618e84
jzhu0601 <accazk1@gmail.com>
2015-11-06 16:13:27 -0500

    WIP

--- app/app.js
+++ app/app.js
@@ -4,5 +4,32 @@
 //IIFE
 (function () {
     "use strict";
-    var app = angular.module('productManagement', []);
+    var app = angular.module('productManagement', ["ui.router"]);
+
+    app.config(["$stateProvider", "$urlRouterProvider", function ($stateProvider, $urlRouterProvider) {
+
+        //default url when first loading the page
+        $urlRouterProvider.otherwise("/");
+
+        $stateProvider
+
+            .state("home", {
+                url: "/",
+                templateUrl: "app/welcomeView.html"
+            })
+
+            .state("productList", {
+                url: "/products",
+                templateUrl: "app/products/productListView.html",
+                controller: "ProductListCtrl"
+            })
+
+            .state("productEdit", {
+                //:productId changes based on actual
+                url: "/products/edit/:productId",
+                templateUrl: "app/products/productEditView.html",
+                controller: "ProductEditCtrl"
+            })
+    }]);
+
 })();
