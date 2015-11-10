25544359ffc377b69865a7d1422c55c6d2d0e870
jzhu0601 <accazk1@gmail.com>
2015-11-09 21:18:49 -0500

    WIP search tags

--- app/products/productEditCtrl.js
+++ app/products/productEditCtrl.js
@@ -7,14 +7,16 @@
         .module("productManagement")
         .controller("ProductEditCtrl", ProductEditCtrl);
 
-    function ProductEditCtrl($scope, $http, productWithSpecificId) {
+    function ProductEditCtrl($scope, $http, productWithSpecificId, $state) {
 
+        $scope.product;
         $http.get("https://raw.githubusercontent.com/DeborahK/AngularLOB/master/product.json").success(function (data) {
 
             for (var i = 0; i < data.length; i++) {
                 var obj = data[i];
                 if (obj.productId == productWithSpecificId) {
                     $scope.product = obj;
+
                     if ($scope.product && $scope.product.productId) {
                         $scope.title = "Edit: " + $scope.product.productName;
                     } else {
@@ -32,11 +34,27 @@
         };
 
         $scope.submit = function () {
-            $scope.product.$save();
+            //Here I do not have anything to save. Will update this later.
+            //$scope.product.$save();
         };
 
         $scope.cancel = function () {
             $state.go('productList');
+        };
+
+        $scope.addTags = function (tags) {
+            if (tags) {
+                var array = tags.split(',');
+                $scope.product.tags = $scope.product.tags ? $scope.product.tags.concat(array) : array;
+                $scope.newTags = "";
+            } else {
+                alert("Enter tags separated by commas");
+            }
+        };
+
+        $scope.removeTag = function (idx) {
+            $scope.product.tags.splice(idx, 1);
         }
+
     }
 })();
